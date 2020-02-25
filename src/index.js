e10fbc025895611ea225e0638019152a754ba865
Renato Pacheco <renato.pacheco.r2@gmail.com>
2020-02-25 01:35:32 -0300

    use react without babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-console.log('index');
-const app = require('./app')
-app.sayHello();
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  React.createElement('h1', null, 'Ola Dev'),
+  document.getElementById("app")
+);
